7eafd13f8bf756f42edb2a5b97b705d6bb582143
Ryan Winchester <ryan@sevenshores.co>
2022-05-06 00:04:18 -0300

    make the elixir dorker work

--- phx-rocks/config/runtime.exs
+++ phx-rocks/config/runtime.exs
@@ -18,17 +18,15 @@ if config_env() == :prod do
   # want to use a different value for prod and you most likely don't want
   # to check this value into version control, so we use an environment
   # variable instead.
-  secret_key_base =
-    System.get_env("SECRET_KEY_BASE") ||
-      raise """
-      environment variable SECRET_KEY_BASE is missing.
-      You can generate one by calling: mix phx.gen.secret
-      """
+  # Normally this would not be hard-coded. However, this is a test application
+  # and it doesn't matter.
+  secret_key_base = "hQKMe1BbumSASasJfByEz7EYqZcvKgykgpeKkrEErg42lKICESBF3/06Afiy271x"
 
   host = System.get_env("PHX_HOST") || "example.com"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :app, AppWeb.Endpoint,
+    check_origin: false,
     url: [host: host, port: 443],
     http: [
       # Enable IPv6 and bind on all interfaces.
