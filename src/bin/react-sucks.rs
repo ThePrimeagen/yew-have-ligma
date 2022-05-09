e04006dcbb8eb7a6b63993a495272c7bee6d69da
mpaulson <mpaulson@netflix.com>
2022-05-09 13:14:05 -0600

    feat: about to write the test client

--- src/bin/react-sucks.rs
+++ src/bin/react-sucks.rs
@@ -1,8 +1,10 @@
+use actix_files::Files;
+use clap::Parser;
 use react_vs_wasm_yew::app::{ReactSucks, AppProps};
 use actix_web::{get, web, App, HttpServer, Responder, HttpResponse};
 
 #[get("/render/{depth}/{girth}")]
-async fn greet(depth: web::Path<(usize, usize)>) -> impl Responder {
+async fn greet(data: web::Data<RenderingData>, depth: web::Path<(usize, usize)>) -> impl Responder {
     let d = depth.0;
     let g = depth.1;
 
@@ -13,17 +15,41 @@ async fn greet(depth: web::Path<(usize, usize)>) -> impl Responder {
     });
 
     let rendered = renderer.render().await;
+    let rendered = data.index_file.replace("__REPLACE_ME_DADDY__", &rendered);
 
     return HttpResponse::Ok()
         .content_type("text/html")
         .body(rendered);
 }
 
+#[derive(Parser)]
+#[clap()]
+struct Args {
+    index_file: String
+}
+
+#[derive(Clone)]
+struct RenderingData {
+    index_file: String
+}
+
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let args = Args::parse();
+    let index_file: Vec<u8> = std::fs::read(args.index_file)?;
+    let index_file: String = std::str::from_utf8(&index_file)
+        .expect("expected utf8 index file")
+        .to_string();
+
+    let data = web::Data::new(RenderingData {
+        index_file,
+    });
+
+    HttpServer::new(move || {
         App::new()
+            .app_data(data.clone())
             .service(greet)
+            .service(Files::new("/", "./dist"))
     })
     .bind(("0.0.0.0", 42069))?
     .run()
