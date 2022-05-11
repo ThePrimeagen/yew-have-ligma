19ad53b13b6399dabd23f566e1a2d43f76146bff
mpaulson <mpaulson@netflix.com>
2022-05-10 20:21:12 -0600

    feat: reverted the promise fin

--- react-sucks/testies.ts
+++ react-sucks/testies.ts
@@ -2,8 +2,6 @@ import * as path from "path";
 import * as fs from "fs";
 import serverMain from "./dist/server/main-server";
 
-const depth = +process.argv[2];
-
 function resolve(p) {
     return path.resolve(__dirname, p);
 }
@@ -13,28 +11,22 @@ const index = fs.
         path.join(__dirname, "dist", "client", "index.html")
     ).toString();
 
-function render(size: number, depth: number): Promise<string> {
-    return new Promise(res => {
-        // @ts-ignore this ignore is for champions and anyone who says differently
-        // will be banned
-        res(index.replace("__REPLACE_ME_DADDY__", serverMain(size, depth)));
-    });
+function render(size: number, depth: number): string {
+    // @ts-ignore this ignore is for champions and anyone who says differently
+    // will be banned
+    return index.replace("__REPLACE_ME_DADDY__", serverMain(size, depth));
 }
 
-async function renderLoop(count: number, size: number, depth: number): Promise<number> {
+function renderLoop(count: number, size: number, depth: number): number {
     const now = Date.now();
     for (let i = 0; i < count; ++i) {
-        await render(size, depth);
+        render(size, depth);
     }
     return Date.now() - now;
 }
 
-async function run() {
-    console.log("100", await renderLoop(100, 20, depth));
-    console.log("1000", await renderLoop(1000, 20, depth));
-    console.log("10000", await renderLoop(10000, 20, depth));
-    console.log("100000", await renderLoop(100000, 20, depth));
-    console.log("1000000", await renderLoop(1000000, 20, depth));
-}
-
-run();
+console.log("100", renderLoop(100, 20, 2));
+console.log("1000", renderLoop(1000, 20, 2));
+console.log("10000", renderLoop(10000, 20, 2));
+console.log("100000", renderLoop(100000, 20, 2));
+console.log("1000000", renderLoop(1000000, 20, 2));
